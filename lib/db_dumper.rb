5af68aafdb805b1c02f8d009f122010e3a6d110b
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-11-24 07:58:13 +0100

    Add logic for creating backup_file

--- lib/db_dumper.rb
+++ lib/db_dumper.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
+#! /usr/bin/env ruby
 
 require_relative "db_dumper/version"
 
 module DbDumper
   class Error < StandardError; end
-  # Your code goes here...
+  sample_databases = {
+    client_one: {
+      database: 'client_one',
+      username: 'client_1',
+      password: 'passwrd'
+    },
+    client_two: {
+      database: 'client_two',
+      username: 'client_2',
+      password: 'passwrd'
+    }
+  }
+
+  end_of_itr = ARGV.shift
+
+  
+  sample_databases.each do |name, config|
+    if end_of_itr.nil?
+      backup_file = config[:sample_databases] + _ + Time.now.strftime('%Y%m%d')
+    else
+      backup_file = config[:sample_databases] + _ + end_of_itr
+    end
+  end
 end
