f7164c538209a8ce295c82024faa3f9df539d6e8
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-11-28 07:40:38 +0100

    Remove sample_databases

--- lib/db_dumper.rb
+++ lib/db_dumper.rb
@@ -5,34 +5,23 @@ require_relative "db_dumper/version"
 
 module DbDumper
   class Error < StandardError; end
-  sample_databases = {
-    client_one: {
-      database: 'client_one',
-      username: 'client_1',
-      password: 'passwrd'
-    },
-    client_two: {
-      database: 'client_two',
-      username: 'client_2',
-      password: 'passwrd'
-    }
-  }
+
+  database = ARGV.shift
+  username = ARGV.shift
+  password = ARGV.shift
 
   database_cmd = ARGV.shift
 
   end_of_itr = ARGV.shift
 
-  
-  sample_databases.each do |name, config|
-    if end_of_itr.nil?
-      backup_file = config[:database] + _ + Time.now.strftime('%Y%m%d')
-    else
-      backup_file = config[:database] + _ + end_of_itr
-    end
-
-    dump_cmd = "#{database_cmd} -u#{config[:username]} -p#{password}" + "#{config[:database]}"
-
-    `#{dump_cmd} > #{backup_file}.sql`
-    `gzip #{backup_file}.sql`
+  if end_of_itr.nil?
+    backup_file = database + Time.now.strftime('%Y%m%d')
+  else
+    backup_file = database + end_of_itr
   end
+
+  dump_cmd = "#{database_cmd} -u#{:username} -p#{password}" + "#{database}"
+
+  `#{dump_cmd} > #{backup_file}.sql`
+  `gzip #{backup_file}.sql`
 end
