97ca681c5e0aa9ae3001ddf8739567f6ec8d6549
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-12-15 08:02:42 +0100

    Run rubocop auto fix

--- lib/db_dumper.rb
+++ lib/db_dumper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
-#! /usr/bin/env ruby
 
-require_relative "db_dumper/version"
+# ! /usr/bin/env ruby
+
+require_relative 'db_dumper/version'
 
 module DbDumper
   class Error < StandardError; end
@@ -14,13 +15,13 @@ module DbDumper
 
   end_of_itr = ARGV.shift
 
-  if end_of_itr.nil?
-    backup_file = database + Time.now.strftime('%Y%m%d')
-  else
-    backup_file = database + end_of_itr
-  end
+  backup_file = if end_of_itr.nil?
+                  database + Time.now.strftime('%Y%m%d')
+                else
+                  database + end_of_itr
+                end
 
-  dump_cmd = "#{database_cmd} -u#{:username} -p#{password}" + "#{database}"
+  dump_cmd = "#{database_cmd} -uusername -p#{password}" + database.to_s
 
   `#{dump_cmd} > #{backup_file}.sql`
   `gzip #{backup_file}.sql`
