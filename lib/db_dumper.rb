1bdbbf6cd43a1964a27af522e93f3f28c9a4a2c0
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-12-19 08:23:39 +0100

    Add option_parser

--- lib/db_dumper.rb
+++ lib/db_dumper.rb
@@ -3,9 +3,29 @@
 # ! /usr/bin/env ruby
 
 require_relative 'db_dumper/version'
+require 'optparse'
 
 module DbDumper
   class Error < StandardError; end
+
+  options = {}
+
+  option_parser = OptionParser.new do |opts|
+    opts.on('-i', '--iteration') do
+      options[:iteration] = true
+    end
+
+    opts.on('-u USER') do |user|
+      options[:user] = user
+    end
+
+    opts.on('-p PASSWORD') do |password|
+      options[:password] = password
+    end
+  end
+
+  option_parser.parse!
+  puts options.inspect
 
   database = ARGV.shift
   username = ARGV.shift
